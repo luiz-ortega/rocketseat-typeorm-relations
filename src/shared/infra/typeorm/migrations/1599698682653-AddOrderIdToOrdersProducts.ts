32e53d93d4ab05f3e55d631c1d6662825f95f90e
luiz-ortega <luizngust@hotmail.com>
2020-09-09 22:28:12 -0300

    feat: order product pivot table migrations.

---
+++ src/shared/infra/typeorm/migrations/1599698682653-AddOrderIdToOrdersProducts.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddOrderIdToOrdersProducts1599698682653
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'order_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrdersProductsOrders',
+        columnNames: ['customer_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'customers',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders_products', 'OrdersProductsOrders');
+
+    await queryRunner.dropColumn('orders_products', 'order_id');
+  }
+}
